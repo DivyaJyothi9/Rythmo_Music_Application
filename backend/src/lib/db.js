62e9cb5b231161a69308b8dad3a1882efc369960
DivyaJyothi9 <liladivyajyothiakula@gmail.com>
2025-04-13 19:54:44 +0530

    project completed

---
+++ backend/src/lib/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDB= async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`Connected to MongoDB ${conn.connection.host}`);
+    } catch ( error) {
+        process.exit(1);
+        console.log("Failed to connect to MongoDB", error);
+        process.exit(1); //1-failure; 0-success
+
+    }
+};
